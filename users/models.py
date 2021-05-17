44b9da890ca3727ec1808795363d0e607f8191bd
smrpn <samarpan_d@pp.iitr.ac.in>
2021-05-17 06:20:09 +0530

    ft: add ocrtext
    Signed-off-by: smrpn <samarpan_d@pp.iitr.ac.in>

--- users/models.py
+++ users/models.py
@@ -6,3 +6,4 @@ class Image(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     image_text = models.CharField(max_length=200)
     image = models.ImageField(upload_to="storage/")
+    OCRtext = models.CharField(max_length=500, default="No text extracted")
--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
+from django.http.response import HttpResponseBadRequest
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm
 from django.contrib.auth.decorators import login_required
 from .forms import UploadFileForm, EditFileName
 from .models import Image
+import cv2
+import pytesseract
 
 
 @login_required
@@ -18,10 +21,22 @@ def home(request):
             username = request.user
             image_text = request.FILES["image"].name
             image = form.cleaned_data.get("image")
+            # extracting text from image
+            print('----------------------')
+            # image_clone = '../media/storage/'+str(image)
+            image_clone = cv2.imread(str(image))
+            print(str(image))
+            print('----------------------')
+            image_clone = cv2.cvtColor(image_clone, cv2.COLOR_BGR2GRAY)
+            image_clone = cv2.threshold(
+                image_clone, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+            image_clone = cv2.medianBlur(image_clone, 5)
+            OCRtext = pytesseract.image_to_string(image_clone)
             obj = Image.objects.create(
                 username=username,
                 image_text=image_text,
-                image=image
+                image=image,
+                OCRtext=OCRtext
             )
             obj.save()
             print(obj)
